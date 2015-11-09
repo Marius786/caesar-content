23123e0fb1bdf4747de89fd6c5a2332690cfbf5e
Marius Olar <marius@presslabs.com>
2015-11-09 12:56:27 +0200

    add new filters to the list

--- caesar-content.php
+++ caesar-content.php
@@ -9,12 +9,17 @@
 
 class Caesar_Content {
 	public function __construct() {
+		add_filter( 'wp_title', array( $this, 'caesar_filer' ), 999, 1 );
+		add_filter( 'wp_title_rss', array( $this, 'caesar_filer' ), 999, 1 );
 		add_filter( 'the_title', array( $this, 'caesar_filer' ), 999, 1 );
+		add_filter( 'the_title_rss', array( $this, 'caesar_filer' ), 999, 1 );
 		add_filter( 'the_content', array( $this, 'caesar_filer' ), 999, 1 );
 		add_filter( 'the_content_feed', array( $this, 'caesar_filer' ), 999, 1 );
 		add_filter( 'widget_title', array( $this, 'caesar_filer' ), 999, 1 );
 		add_filter( 'widget_text', array( $this, 'caesar_filer' ), 999, 1 );
 		add_filter( 'get_comment_text', array( $this, 'caesar_filer' ), 999, 1 );
+		add_filter( 'bloginfo', array( $this, 'caesar_filer' ), 999, 1 );
+		add_filter( 'the_excerpt', array( $this, 'caesar_filer' ), 999, 1 );
 	}
 
 	public function caesar_filer( $text ) {
